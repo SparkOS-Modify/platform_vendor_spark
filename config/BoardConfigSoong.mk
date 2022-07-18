DerpFest-12/vendor_derp

vendor: soong: Add camera_needs_client_info_lib

config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += derpGlobalVars
 SOONG_CONFIG_derpGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -64,6 +65,7 @@ SOONG_CONFIG_derpQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_derpGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_derpGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_derpGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_derpGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)

SparkOS-Modify/platform_vendor_spark

config/BoardConfigSoong.mk
@@ -35,6 +35,7 @@ SOONG_CONFIG_sparkGlobalVars += \
     has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
+    camera_needs_client_info_lib \
     target_inputdispatcher_skip_event_key \
     target_init_vendor_lib \
     target_ld_shim_libs \
@@ -73,6 +74,7 @@ SOONG_CONFIG_sparkGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET
 SOONG_CONFIG_sparkGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_sparkGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_sparkGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
+SOONG_CONFIG_sparkGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_sparkGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_sparkNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_sparkQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)